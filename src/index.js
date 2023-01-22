4960ad5c3820caaadf702dbdb65a42c082414364
Maryna-Korbet <m22v86k@gmail.com>
2023-01-22 10:36:13 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { StrictMode } from "react";
+import 'modern-normalize';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
